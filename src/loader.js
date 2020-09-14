a2b187965afae6237f9b2ea01cab512a8b54062d
khiemkino <khiemkino@gmail.com>
2020-09-14 20:52:54 +0700

    newUpdate

---
+++ src/loader.js
@@ -0,0 +1,169 @@
+// @flow
+
+import * as BufferLayout from 'buffer-layout';
+
+import {Account} from './account';
+import {PublicKey} from './publickey';
+import {Transaction, PACKET_DATA_SIZE} from './transaction';
+import {SYSVAR_RENT_PUBKEY} from './sysvar';
+import {sendAndConfirmTransaction} from './util/send-and-confirm-transaction';
+import {sleep} from './util/sleep';
+import type {Connection} from './connection';
+import {SystemProgram} from './system-program';
+
+/**
+ * Program loader interface
+ */
+export class Loader {
+  /**
+   * Amount of program data placed in each load Transaction
+   */
+  static get chunkSize(): number {
+    // Keep program chunks under PACKET_DATA_SIZE, leaving enough room for the
+    // rest of the Transaction fields
+    //
+    // TODO: replace 300 with a proper constant for the size of the other
+    // Transaction fields
+    return PACKET_DATA_SIZE - 300;
+  }
+
+  /**
+   * Minimum number of signatures required to load a program not including
+   * retries
+   *
+   * Can be used to calculate transaction fees
+   */
+  static getMinNumSignatures(dataLength: number): number {
+    return Math.ceil(dataLength / Loader.chunkSize);
+  }
+
+  /**
+   * Loads a generic program
+   *
+   * @param connection The connection to use
+   * @param payer System account that pays to load the program
+   * @param program Account to load the program into
+   * @param programId Public key that identifies the loader
+   * @param data Program octets
+   */
+  static async load(
+    connection: Connection,
+    payer: Account,
+    program: Account,
+    programId: PublicKey,
+    data: Buffer | Uint8Array | Array<number>,
+  ): Promise<void> {
+    {
+      const balanceNeeded = await connection.getMinimumBalanceForRentExemption(
+        data.length,
+      );
+      const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+          fromPubkey: payer.publicKey,
+          newAccountPubkey: program.publicKey,
+          lamports: balanceNeeded > 0 ? balanceNeeded : 1,
+          space: data.length,
+          programId,
+        }),
+      );
+      await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer, program],
+        {
+          commitment: 'single',
+          skipPreflight: true,
+        },
+      );
+    }
+
+    const dataLayout = BufferLayout.struct([
+      BufferLayout.u32('instruction'),
+      BufferLayout.u32('offset'),
+      BufferLayout.u32('bytesLength'),
+      BufferLayout.u32('bytesLengthPadding'),
+      BufferLayout.seq(
+        BufferLayout.u8('byte'),
+        BufferLayout.offset(BufferLayout.u32(), -8),
+        'bytes',
+      ),
+    ]);
+
+    const chunkSize = Loader.chunkSize;
+    let offset = 0;
+    let array = data;
+    let transactions = [];
+    while (array.length > 0) {
+      const bytes = array.slice(0, chunkSize);
+      const data = Buffer.alloc(chunkSize + 16);
+      dataLayout.encode(
+        {
+          instruction: 0, // Load instruction
+          offset,
+          bytes,
+        },
+        data,
+      );
+
+      const transaction = new Transaction().add({
+        keys: [{pubkey: program.publicKey, isSigner: true, isWritable: true}],
+        programId,
+        data,
+      });
+      transactions.push(
+        sendAndConfirmTransaction(connection, transaction, [payer, program], {
+          commitment: 'single',
+          skipPreflight: true,
+        }),
+      );
+
+      // Delay between sends in an attempt to reduce rate limit errors
+      const REQUESTS_PER_SECOND = 4;
+      await sleep(1000 / REQUESTS_PER_SECOND);
+
+      // Run up to 8 Loads in parallel to prevent too many parallel transactions from
+      // getting retried due to AccountInUse errors.
+      //
+      // TODO: 8 was selected empirically and should probably be revisited
+      if (transactions.length === 8) {
+        await Promise.all(transactions);
+        transactions = [];
+      }
+
+      offset += chunkSize;
+      array = array.slice(chunkSize);
+    }
+    await Promise.all(transactions);
+
+    // Finalize the account loaded with program data for execution
+    {
+      const dataLayout = BufferLayout.struct([BufferLayout.u32('instruction')]);
+
+      const data = Buffer.alloc(dataLayout.span);
+      dataLayout.encode(
+        {
+          instruction: 1, // Finalize instruction
+        },
+        data,
+      );
+
+      const transaction = new Transaction().add({
+        keys: [
+          {pubkey: program.publicKey, isSigner: true, isWritable: true},
+          {pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false},
+        ],
+        programId,
+        data,
+      });
+      await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer, program],
+        {
+          commitment: 'single',
+          skipPreflight: true,
+        },
+      );
+    }
+  }
+}
---
+++ test/bpf-loader.test.js
@@ -0,0 +1,199 @@
+// @flow
+
+import fs from 'mz/fs';
+
+import {
+  Connection,
+  BpfLoader,
+  Transaction,
+  sendAndConfirmTransaction,
+  Account,
+} from '../src';
+import {mockRpcEnabled} from './__mocks__/node-fetch';
+import {url} from './url';
+import {newAccountWithLamports} from './new-account-with-lamports';
+import {BPF_LOADER_PROGRAM_ID} from '../src/bpf-loader';
+
+if (!mockRpcEnabled) {
+  // The default of 5 seconds is too slow for live testing sometimes
+  jest.setTimeout(120000);
+}
+
+const NUM_RETRIES = 100; /* allow some number of retries */
+
+test('load BPF C program', async () => {
+  if (mockRpcEnabled) {
+    console.log('non-live test skipped');
+    return;
+  }
+
+  const data = await fs.readFile('test/fixtures/noop-c/noop.so');
+
+  const connection = new Connection(url, 'recent');
+  const {feeCalculator} = await connection.getRecentBlockhash();
+  const fees =
+    feeCalculator.lamportsPerSignature *
+    (BpfLoader.getMinNumSignatures(data.length) + NUM_RETRIES);
+  const balanceNeeded = await connection.getMinimumBalanceForRentExemption(
+    data.length,
+  );
+  const from = await newAccountWithLamports(connection, fees + balanceNeeded);
+
+  const program = new Account();
+  await BpfLoader.load(connection, from, program, data, BPF_LOADER_PROGRAM_ID);
+  const transaction = new Transaction().add({
+    keys: [{pubkey: from.publicKey, isSigner: true, isWritable: true}],
+    programId: program.publicKey,
+  });
+  await sendAndConfirmTransaction(connection, transaction, [from], {
+    commitment: 'single',
+    skipPreflight: true,
+  });
+});
+
+describe('load BPF Rust program', () => {
+  if (mockRpcEnabled) {
+    console.log('non-live test skipped');
+    return;
+  }
+
+  const connection = new Connection(url, 'recent');
+
+  let program: Account;
+  let signature: string;
+  let payerAccount: Account;
+
+  beforeAll(async () => {
+    const data = await fs.readFile(
+      'test/fixtures/noop-rust/solana_bpf_rust_noop.so',
+    );
+
+    const {feeCalculator} = await connection.getRecentBlockhash();
+    const fees =
+      feeCalculator.lamportsPerSignature *
+      (BpfLoader.getMinNumSignatures(data.length) + NUM_RETRIES);
+    const balanceNeeded = await connection.getMinimumBalanceForRentExemption(
+      data.length,
+    );
+
+    payerAccount = await newAccountWithLamports(
+      connection,
+      fees + balanceNeeded,
+    );
+
+    program = new Account();
+    await BpfLoader.load(
+      connection,
+      payerAccount,
+      program,
+      data,
+      BPF_LOADER_PROGRAM_ID,
+    );
+
+    const transaction = new Transaction().add({
+      keys: [
+        {pubkey: payerAccount.publicKey, isSigner: true, isWritable: true},
+      ],
+      programId: program.publicKey,
+    });
+
+    signature = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [payerAccount],
+      {
+        skipPreflight: true,
+      },
+    );
+  });
+
+  test('get confirmed transaction', async () => {
+    const parsedTx = await connection.getParsedConfirmedTransaction(signature);
+    if (parsedTx === null) {
+      expect(parsedTx).not.toBeNull();
+      return;
+    }
+    const {signatures, message} = parsedTx.transaction;
+    expect(signatures[0]).toEqual(signature);
+    const ix = message.instructions[0];
+    if (ix.parsed) {
+      expect('parsed' in ix).toBe(false);
+    } else {
+      expect(ix.programId.equals(program.publicKey)).toBe(true);
+      expect(ix.data).toEqual('');
+    }
+  });
+
+  test('simulate transaction', async () => {
+    const simulatedTransaction = new Transaction().add({
+      keys: [
+        {pubkey: payerAccount.publicKey, isSigner: true, isWritable: true},
+      ],
+      programId: program.publicKey,
+    });
+
+    const {err, logs} = (
+      await connection.simulateTransaction(simulatedTransaction, [payerAccount])
+    ).value;
+    expect(err).toBeNull();
+
+    if (logs === null) {
+      expect(logs).not.toBeNull();
+      return;
+    }
+
+    expect(logs.length).toBeGreaterThanOrEqual(2);
+    expect(logs[0]).toEqual(`Call BPF program ${program.publicKey.toBase58()}`);
+    expect(logs[logs.length - 1]).toEqual(
+      `BPF program ${program.publicKey.toBase58()} success`,
+    );
+  });
+
+  test('simulate transaction without signature verification', async () => {
+    const simulatedTransaction = new Transaction().add({
+      keys: [
+        {pubkey: payerAccount.publicKey, isSigner: true, isWritable: true},
+      ],
+      programId: program.publicKey,
+    });
+
+    simulatedTransaction.setSigners(payerAccount.publicKey);
+    const {err, logs} = (
+      await connection.simulateTransaction(simulatedTransaction)
+    ).value;
+    expect(err).toBeNull();
+
+    if (logs === null) {
+      expect(logs).not.toBeNull();
+      return;
+    }
+
+    expect(logs.length).toBeGreaterThanOrEqual(2);
+    expect(logs[0]).toEqual(`Call BPF program ${program.publicKey.toBase58()}`);
+    expect(logs[logs.length - 1]).toEqual(
+      `BPF program ${program.publicKey.toBase58()} success`,
+    );
+  });
+
+  test('simulate transaction with bad programId', async () => {
+    const simulatedTransaction = new Transaction().add({
+      keys: [
+        {pubkey: payerAccount.publicKey, isSigner: true, isWritable: true},
+      ],
+      programId: new Account().publicKey,
+    });
+
+    simulatedTransaction.setSigners(payerAccount.publicKey);
+    const {err, logs} = (
+      await connection.simulateTransaction(simulatedTransaction)
+    ).value;
+    expect(err).toEqual('ProgramAccountNotFound');
+
+    if (logs === null) {
+      expect(logs).not.toBeNull();
+      return;
+    }
+
+    expect(logs.length).toEqual(0);
+  });
+});
